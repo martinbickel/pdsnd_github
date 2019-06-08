80d4af57d953d52063fca81a107f3cffa2d02a12
Martin Bickel <martin.mb.bickel@gmx.de>
2019-06-08 15:01:21 +0200

    docs: Improvement to readability in data loading section

--- bikeshare_project.py
+++ bikeshare_project.py
@@ -79,7 +79,7 @@ def load_data(city, month, day, decision_filter):
     Loads data for the specified city and filters by month and day if applicable.
 
     Args:
-        (str) city - name of the city to analyze
+        (str) city - name of the city to analyze (Either, Washington, Chicago or New York City)
         (str) month - name of the month to filter by, or "all" to apply no month filter
         (str) day - name of the day of week to filter by, or "all" to apply no day filter
     Returns:
